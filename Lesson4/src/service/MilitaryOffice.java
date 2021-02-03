cdb43bb7c20947d83f936ebf9ebcb341cd7b44d4
Andrew Degtiarov <andrew.degtiarov@gmail.com>
2021-02-03 14:19:11 +0300

    I guess, I understood ))

--- Lesson4/src/service/MilitaryOffice.java
+++ Lesson4/src/service/MilitaryOffice.java
@@ -15,10 +15,8 @@ public class MilitaryOffice {
     public String suitable(Address address) {
         StringBuilder names = new StringBuilder();
         System.out.println("who is fit for the army: ");
-        for (Person person : personRegistry.getCitizens()) {
-            if (person.getAddress().getCountry().equals(address.getCountry()) &&
-                    person.getAddress().getCity().equals(address.getCity()) &&
-                    person.getSex().equals("man") &&
+        for (Person person : personRegistry.getPeople(address)) {
+            if (person.getSex().equals("man") &&
                     person.getAge() > 17 && person.getAge() < 28) {
                 names.append(person.getName()).append("\n");
             }
@@ -42,10 +40,8 @@ public class MilitaryOffice {
     public String suitableToHeight(Address address, Integer height) {
         StringBuilder names = new StringBuilder();
         System.out.println("who is fit for the army with a height of more than: ");
-        for (Person person : personRegistry.getCitizens()) {
-            if (person.getAddress().getCountry().equals(address.getCountry()) &&
-                    person.getAddress().getCity().equals(address.getCity()) &&
-                    person.getSex().equals("man") &&
+        for (Person person : personRegistry.getPeople(address)) {
+            if (person.getSex().equals("man") &&
                     person.getAge() > 17 && person.getAge() < 28 &&
                     person.getHeight() >= height) {
                 names.append(person.getName()).append("\n");
